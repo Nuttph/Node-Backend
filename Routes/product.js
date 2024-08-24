4e84e663bbf24e980a79049aaacdc3129f9c3fd7
Nuttaphon <131658916+Nuttph@users.noreply.github.com>
2024-08-24 23:11:31 +0700

    update middleware

--- Routes/product.js
+++ Routes/product.js
@@ -5,4 +5,20 @@ router.get('/product', (req, res) => {
     res.send('Hello Product Endpoint')
 });
 
+//ส่ง ID
+router.get('/product/:id', (req, res) => {
+    res.send('Hello 1 Product')
+});
+
+router.post('/product', (req, res) => {
+    res.send('Hello Post')
+})
+
+router.put('/product/:id', (req, res) => {
+    res.send('hello id')
+})
+
+router.delete('/product', (req, res) => {
+    res.json({name:"Hello Nut"})
+})
 module.exports = router //ให้ไฟล์อื่นสามารถใช้ file นี้ได้
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express') //รับ express
+
+// reuire fs เอา readdirSync เอาอ่าน All 7Routers
+const {readdirSync} = require('fs')
+
+
 //เรียก Routes มาใช้
 const productRouters = require('./Routes/product')
-
+const authRouters = require('./Routes/auth')
 
 const app = express(); //สร้างตัวแปรมา run express
 
@@ -13,9 +18,17 @@ const app = express(); //สร้างตัวแปรมา run express
 //     // res.send('Hello world')
 // }) ถ้าเรามี enpoint เยอะๆ
 
-// Rout 2
-// สร้าง folder Routes
-app.use('/api',productRouters) //จะเป็น part /api/product
 
+// Route 2
+// สร้าง folder Routes
+// app.use('/api',productRouters) //จะเป็น part /api/product
+// app.use('/api',authRouters) // สร้างอีกละ
+
+//Route 3 auto Endpoint
+
+readdirSync('./Routes').map((r) => {
+    // console.log(`this is "${r}"`) // auth.js product.js
+    app.use('/api',require(`./Routes/${r}`))
+})
 
 app.listen(5000, ()=>{console.log("Server Running on port 5000")}) //ตัว runserver (เลข port , call back function)
